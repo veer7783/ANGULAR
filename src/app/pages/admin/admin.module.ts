4cb318871de60b5790b248eb8ef3ff86a587d655
veer <veer@gmail.com>
2022-09-13 12:46:33 +0530

    this is first commit

---
+++ src/app/pages/admin/admin.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { FormsModule } from '@angular/forms';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
